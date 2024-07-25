19afdcc61c33c466ebd984273ea681630eb140c8
leesecre <wodydchlrh@gmail.com>
2024-07-25 13:52:52 +0900

    Add new config, qemu setting

--- qemu/run_vm.sh
+++ qemu/run_vm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 QEMU_SSH_PORT=5555
 QEMU_GDB_PORT=1235
-TOTAL_MEM="76G"
+TOTAL_MEM="64G"
 NVME_PCIE_ADDR="" # Set proper PCIE address for your NVMe device.
 # NVME_PCIE_ADDR="d8:00.0"
 # NVME_PCIE_ADDR="af:00.0"
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then # script is executed directly.
 	qemu-system-x86_64	-kernel ../djournalplus-kernel.code/arch/x86/boot/bzImage \
-				-initrd /boot/initrd.img-6.2.10-djplus+ \
+				-initrd /boot/initrd.img-6.2.10djplus+ \
 				-cpu host \
 				-smp cpus=32 \
 				-drive file=vm_imgs/qemu-image.qcow2,index=0,media=disk,format=qcow2 \
